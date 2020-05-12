b7e159d9f45958a566e9a731cf92cd77d50ee973
Julua <juliayrkina@gmail.com>
2020-05-12 21:58:35 +0300

    Updates

---
+++ precache-manifest.e01b47dc2265f313e042bc55f037aee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75231dcd00e2c5b5fcbdcada41036d89",
+    "url": "/react-bookstore/index.html"
+  },
+  {
+    "revision": "01965cba92dadb0b6006",
+    "url": "/react-bookstore/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a88e0187f56be52e6a1d",
+    "url": "/react-bookstore/static/css/main.ec9bf303.chunk.css"
+  },
+  {
+    "revision": "01965cba92dadb0b6006",
+    "url": "/react-bookstore/static/js/2.977cc6aa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-bookstore/static/js/2.977cc6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88e0187f56be52e6a1d",
+    "url": "/react-bookstore/static/js/main.d2d850c9.chunk.js"
+  },
+  {
+    "revision": "c029db64e57a01180b85",
+    "url": "/react-bookstore/static/js/runtime-main.23908500.js"
+  }
+]);
